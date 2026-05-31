a568962d71d2e45003870500a34bc144d45580c1
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2026-05-31 21:36:59 +0000

    Codion upgraded

--- models/ollama/llemmy/build.gradle.kts
+++ models/ollama/llemmy/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     // The Badass Jlink Plugin provides jlink and jpackage
     // functionality and applies the java application plugin
     // https://badass-jlink-plugin.beryx.org
-    id("org.beryx.jlink") version "4.0.0"
+    id("org.beryx.jlink") version "4.0.2"
     id("org.gradlex.extra-java-module-info") version "1.14"
 }
 
